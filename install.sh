bdf06748616949b2c77485dd58850ac406f26adb
X Faker <xox.xfaker@gmail.com>
2025-09-13 23:38:22 +0330

    feat: synx with tx-ui

--- install.sh
+++ install.sh
@@ -7,12 +7,12 @@ else
     source /usr/lib/os-release
     release=$ID
 fi  
-rm -rf 3xui-multi-protocol
+rm -rf tx-ui-multi-protocol
 
 if [[ "${release}" == "debian" ]]; then
 apt install git
-git clone https://github.com/M4mmad/3xui-multi-protocol.git
-  cd 3xui-multi-protocol/3xui-multi-protocol
+git clone https://github.com/XFaker-XOX/tx-ui-multi-protocol.git
+  cd tx-ui-multi-protocol/tx-ui-multi-protocol
   wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb 
   dpkg -i packages-microsoft-prod.deb 
   rm packages-microsoft-prod.deb 
@@ -21,30 +21,30 @@ git clone https://github.com/M4mmad/3xui-multi-protocol.git
   apt-get install -y aspnetcore-runtime-7.0 
   apt-get install -y dotnet-runtime-7.0 
   apt install libc6 
-  dotnet publish -c Release -o /etc/3xui-multi-protocol
+  dotnet publish -c Release -o /etc/tx-ui-multi-protocol
 elif [[ "${release}" == "centos" ]]; then 
 dnf install wget
 dnf install git
-git clone https://github.com/M4mmad/3xui-multi-protocol.git
- cd 3xui-multi-protocol/3xui-multi-protocol
+git clone https://github.com/XFaker-XOX/tx-ui-multi-protocol.git
+ cd tx-ui-multi-protocol/tx-ui-multi-protocol
   sudo dnf install dotnet-sdk-7.0
   sudo dnf install aspnetcore-runtime-7.0
   sudo dnf install dotnet-runtime-7.0
-  dotnet publish -c Release -o /etc/3xui-multi-protocol
+  dotnet publish -c Release -o /etc/tx-ui-multi-protocol
 elif [[ "${release}" == "fedora" ]]; then
 dnf install wget
 dnf install git
-git clone https://github.com/M4mmad/3xui-multi-protocol.git
- cd 3xui-multi-protocol/3xui-multi-protocol
+git clone https://github.com/XFaker-XOX/tx-ui-multi-protocol.git
+ cd tx-ui-multi-protocol/tx-ui-multi-protocol
   sudo dnf install dotnet-sdk-7.0
   sudo dnf install aspnetcore-runtime-7.0
   sudo dnf install dotnet-runtime-7.0
-  dotnet publish -c Release -o /etc/3xui-multi-protocol
+  dotnet publish -c Release -o /etc/tx-ui-multi-protocol
 elif [[ "${release}" == "ubuntu" ]]; then
 declare repo_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
 apt install git
-git clone https://github.com/M4mmad/3xui-multi-protocol.git
- cd 3xui-multi-protocol/3xui-multi-protocol
+git clone https://github.com/XFaker-XOX/tx-ui-multi-protocol.git
+ cd tx-ui-multi-protocol/tx-ui-multi-protocol
  wget https://packages.microsoft.com/config/ubuntu/$repo_version/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
  dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
@@ -60,26 +60,26 @@ sudo apt-get purge dotnet-sdk-7.0
 sudo rm -f /etc/apt/sources.list.d/microsoft-prod.list && sudo apt update
 sudo apt install -y dotnet7
 fi
-  dotnet publish -c Release -o /etc/3xui-multi-protocol
+  dotnet publish -c Release -o /etc/tx-ui-multi-protocol
 
  else
  apt install git
-git clone https://github.com/M4mmad/3xui-multi-protocol.git
+git clone https://github.com/XFaker-XOX/tx-ui-multi-protocol.git
   wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
   chmod +x ./dotnet-install.sh
   ./dotnet-install.sh --channel 7.0
   apt update
   apt install -y dotnet7
-  cd 3xui-multi-protocol/3xui-multi-protocol
-  dotnet publish -c Release -o /etc/3xui-multi-protocol
+  cd tx-ui-multi-protocol/tx-ui-multi-protocol
+  dotnet publish -c Release -o /etc/tx-ui-multi-protocol
 fi
 
   
  
   cd /etc/systemd/system/
-  wget https://raw.githubusercontent.com/M4mmad/3xui-multi-protocol/master/3xui-multi-protocol.service
+  wget https://raw.githubusercontent.com/XFaker-XOX/tx-ui-multi-protocol/master/tx-ui-multi-protocol.service
    cp /etc/x-ui/x-ui.db /etc/x-ui/backup.db
   systemctl daemon-reload
-  systemctl start 3xui-multi-protocol
-systemctl enable 3xui-multi-protocol
-systemctl restart 3xui-multi-protocol
+  systemctl start tx-ui-multi-protocol
+systemctl enable tx-ui-multi-protocol
+systemctl restart tx-ui-multi-protocol
--- unistall.sh
+++ unistall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-    systemctl stop 3xui-multi-protocol
-    systemctl disable 3xui-multi-protocol
-    rm -f /etc/systemd/system/3xui-multi-protocol.service 
+    systemctl stop tx-ui-multi-protocol
+    systemctl disable tx-ui-multi-protocol
+    rm -f /etc/systemd/system/tx-ui-multi-protocol.service 
     systemctl daemon-reload
     systemctl reset-failed
-    rm -rf /etc/3xui-multi-protocol 
-    rm -rf 3xui-multi-protocol/3xui-multi-protocol 
+    rm -rf /etc/tx-ui-multi-protocol 
+    rm -rf tx-ui-multi-protocol/tx-ui-multi-protocol 
     sudo apt-get purge dotnet-sdk-7.0
      sudo rm -f /etc/apt/sources.list.d/microsoft-prod.list && sudo apt update
 
